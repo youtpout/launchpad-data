815d4cfa7c4bacbeca41e2278502a325ce9b4b7f
Eddy Boughioul <be@be-technology.fr>
2022-03-08 09:52:42 +0100

    abi

---
+++ abi/contractAbi.js
@@ -0,0 +1,21 @@
+export const contractAbi = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "isContract",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
